ad50c2f86b487cc0f1ba85218e5f5e5b1d7f5977
Djura22 <michaeliangroves@gmail.com>
2020-08-06 12:46:18 +0100

    BankAccount class created and test for balance method added

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,4 +1,12 @@
-describe BankAccount do 
+require 'bank_account'
+
+describe BankAccount do
+	
+	describe 'balance' do
+		it 'BankAccount responds to balance method' do
+			expect(subject).to respond_to('balance')
+		end
+	end
 
 end
 
