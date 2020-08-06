8f93d9d38c547876ba425413415d71abffba3af9
Djura22 <michaeliangroves@gmail.com>
2020-08-06 12:52:34 +0100

    balance method added to comply with test and new test written for expected output of balance

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,7 @@
 class BankAccount
 
+  def balance
+
+  end
+
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,6 +6,9 @@ describe BankAccount do
 		it 'BankAccount responds to balance method' do
 			expect(subject).to respond_to('balance')
 		end
+		it 'balance is returned as integer when called on BankAccount' do
+		  expect(subject.balance).to eq(0)
+		end
 	end
 
 end
